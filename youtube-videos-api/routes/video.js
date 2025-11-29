43a8c248b94a415213142db46b441007694f921d
AlexFranciscoDev <alexbcn98@gmail.com>
2025-11-30 00:22:27 +0100

    feat: get videos by platform and category working, missing test

--- youtube-videos-api/routes/video.js
+++ youtube-videos-api/routes/video.js
@@ -6,6 +6,7 @@ const upload = require('../middlewares/upload');
 
 router.post('/', auth.isAuth, upload.single('image'), VideoController.postVideo);
 router.get("/", auth.isAuth, VideoController.listVideos);
+router.get('/filter', auth.isAuth, VideoController.getVideosByPlatformAndCategory);
 router.get('/category/:category', auth.isAuth, VideoController.getVideosByCategory);
 router.get('/platform/:platform', auth.isAuth, VideoController.getVideosByPlatform);
 router.get('/user/:id', auth.isAuth, VideoController.getVideosByUser);
