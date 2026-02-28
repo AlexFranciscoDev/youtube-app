1e45f45880f9466439687f1dcbad2919457cbdce
alexfranciscodev <alexbcn98@gmail.com>
2026-02-28 22:22:38 +0100

    docker configuration

--- youtube-videos-api/database/conection.js
+++ youtube-videos-api/database/conection.js
@@ -1,14 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const conection = async () => {
-    try {
-        await mongoose.connect('mongodb://localhost:27017/youtube-app');
-        console.log('Youtube app database connected successfully');
-    } catch (error) {
-        console.error('Database connection failed:', error);
-    }
-}
+  try {
+    const mongoUri = process.env.MONGO_URI || "mongodb://localhost:27017/youtube-app";
+    await mongoose.connect(mongoUri);
+    console.log("Youtube app database connected successfully");
+    // await mongoose.connect('mongodb://localhost:27017/youtube-app');
+    // console.log('Youtube app database connected successfully');
+  } catch (error) {
+    console.error("Database connection failed:", error);
+  }
+};
 
 module.exports = {
-    conection
-}
+  conection,
+};
--- youtube-videos-api/index.js
+++ youtube-videos-api/index.js
@@ -1,4 +1,5 @@
 // Dependencies
+require("dotenv").config();
 const express = require('express');
 const cors = require('cors');
 const { conection } = require('./database/conection');
